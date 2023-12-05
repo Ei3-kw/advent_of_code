ee1d9d3c4db31bf739166b4c6371da99b1db6813
Ei3-kw <52182320+Ei3-kw@users.noreply.github.com>
2023-12-05 21:25:42 +1000

    day5
    bruh

---
+++ 2023/day5/day5.py
@@ -0,0 +1,40 @@
+def map(t, x):
+    for dst, src, n in t:
+        if src <= x <src+n:
+            return x + dst - src
+    return x
+
+def mapping(t, r):
+    bruh = []
+    for dst, src, n in t:
+        new_r = []
+        while r:
+            stt, end = r.pop()
+            b = (stt, min(end, src))
+            i = (max(stt, src), min(src + n, end))
+            a = (max(src + n, stt), end)
+            new_r.extend(filter(lambda x: x[1] > x[0], [b, a]))
+            bruh.append((i[0] - src + dst, i[1] - src + dst)) if i[1] > i[0] else None
+        r = new_r
+    return bruh + r
+
+
+if __name__ == '__main__':
+    seed, *others = open('5.txt').read().split('\n\n')
+    seed = [int(x) for x in seed.split(':')[1].split()]
+
+    p1 = []
+    for x in seed:
+        for pt in others:
+            x = map([[int(x) for x in line.split()] for line in pt.split('\n')[1:]], x)
+        p1.append(x)
+    print(min(p1))
+
+    p2 = []
+    for m, n in list(zip(seed[::2], seed[1::2])):
+        x = [(m, m+n)]
+        for pt in others:
+            x = mapping([[int(x) for x in line.split()] for line in pt.split('\n')[1:]], x)
+        p2.append(min(x))
+    print(min(p2)[0])
+
