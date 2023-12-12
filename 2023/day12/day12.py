01a8a7c54f700e25e0f13e83c4450c786d422c8c
Ei3 <e013.kw@gmail.com>
2023-12-13 10:26:35 +1100

    Update day12.py

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,18 +1,19 @@
 from functools import cache
+from time import time
+
+start = time()
 
 *lines, = open('12.txt')
 
-def possibility(l, r):
-    return r \
+C = cache(lambda l,r: r \
     and (c:=r[0]) \
     and ~sum(
         ~C(l[i+c+1:], r[1:]) 
             for i in range(len(l)-c+1) 
                 if {'#'} - {l[i+c: i+c+1], *l[:i]}
                     and {'.'} - {*l[i:i+c]}) \
-    or ~('#'in l)^1
-
-C = cache(lambda l,r: possibility(l, r))
+    or ~('#'in l)^1)
 
 for n in 1, 5:
     print(sum(~C('?'.join(n*[l]), eval(r)*n) for l,r in map(str.split, lines)))
+    print(time()-start)
